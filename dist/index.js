ec620ae7f18d10c33c0387425581d46c7764d1a4
Ndeta <ndetainnocent3@gmail.com>
2023-07-03 12:05:19 +0300

    typescript crash

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,62 @@
 "use strict";
+//Basic types 
 let id = 5;
+let company = "Ndeta Innocent";
+let isPublished = true;
+let x = 'Hello';
+let ids = [1, 2, 3, 4, 5];
+//Tuple
+let person = [1, 'Ndeta', true];
+//Tuple array
+let employee;
+employee = [
+    [1, 'Ndeta'],
+    [2, "Romance"]
+];
+//Unions
+let pid;
+pid = 9;
+//Enums
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up);
+const user = {
+    id: 1,
+    name: 'John'
+};
+//Type assertion
+let cid = 1;
+let customerId = cid;
+//Functions
+function addNum(x, y) {
+    return x + y;
+}
+console.log(addNum(3, 67));
+const user2 = {
+    id: 43,
+    name: 'Ndeta'
+};
+//Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const ndeta = new Person(1, "Innocent");
+ndeta.register();
+//Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4, 5]);
+let strArray = getArray(['Ndeta', 'Romance', 'John']);
 console.log("ID", id);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,75 @@
+//Basic types 
 let id:number=5
+let company:string ="Ndeta Innocent"
+let isPublished:boolean=true
+let x:any='Hello'
+let ids:number[]=[1,2,3,4,5]
+//Tuple
+let person:[number, string, boolean]=[1,'Ndeta',true ]
+//Tuple array
+let employee:[number,string][]
+employee=[
+    [1,'Ndeta'],
+    [2,"Romance"]
+]
+//Unions
+let pid:string |number
+pid=9
 
+//Enums
+enum Direction1{
+    Up,
+    Down,
+    Left,
+    Right
+}
+console.log(Direction1.Up);
+//Objects
+type User={
+    id:number,
+    name:string
+}
+const user:User={
+    id:1,
+    name:'John'
+}
+//Type assertion
+let cid:any=1
+let customerId=<number>cid
+
+//Functions
+function addNum(x:number,y:number):number{
+    return x+y
+}
+console.log(addNum(3,67));
+//Interface
+interface UserInterface{
+    id:number,
+    name:string
+}
+const user2:UserInterface={
+    id:43,
+    name:'Ndeta'
+}
+//Classes
+class Person{
+    id:number
+    name:string
+
+    constructor(id:number, name:string){
+        this.id=id
+        this.name=name
+    }
+    register(){
+        return `${this.name} is now registered`
+    }
+}
+const ndeta=new Person(1,"Innocent")
+ndeta.register()
+//Generics
+function getArray<T>(items:T[]):T[]{
+    return new Array().concat(items)
+}
+let numArray=getArray<number>([1,2,3,4,5])
+let strArray=getArray<string>(['Ndeta','Romance','John'])
 console.log("ID", id);
